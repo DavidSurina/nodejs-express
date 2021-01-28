907afa5e26401a07c21cb5f07a33517c6ebbd6bc
memo40 <memocann4040@gmail.com>
2021-01-28 15:21:35 +0100

    set up postby id

--- routes/postbyid/index.js
+++ routes/postbyid/index.js
@@ -1,1 +1,8 @@
-'test'
+const express= require('express');
+const router=express.Router();
+
+const getPostById=require('../../controllers/postbyid');
+
+router.get('/postbyid',getPostById.logPostById);
+
+module.exports=router;
