97bba9fcc8f4fba66c8870289aec7320faf6fef6
olkhon <alexander.stapel@gmail.com>
2021-01-28 16:17:42 +0100

    post by title routing

---
+++ controllers/postByTitle.js
@@ -0,0 +1,5 @@
+const getPostByTitle = {
+    logPostByTitle: (req, res) => res.send('title test'),
+  };
+
+  module.exports = getPostByTitle;
--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require("express");
 const app = express();
 const allPosts = require("./routes/allposts/index");
 const postById = require("./routes/postbyid/index");
-const postByRating = require("./routes/postbyrating/index")
+const postByRating = require("./routes/postbyrating/index");
+const postByTitle = require("./routes/postbytitle/index");
 const port = 3000;
 
 app.get("/", (req, res) => res.send("11313"));
 app.get("/allPosts", allPosts);
 app.get("/postbyid",postById);
 app.get("/postbyrating", postByRating);
+app.get("/postbytitle", postByTitle);
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
