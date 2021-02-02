396a6afd80152305e5ab35a9b243a121904e1742
olkhon <alexander.stapel@gmail.com>
2021-02-02 17:45:17 +0100

    update

--- controllers/allPosts.js
+++ controllers/allPosts.js
@@ -6,12 +6,12 @@ const getAllPosts = {
     try {
       const data = await pool.query("SELECT * FROM posts");
       console.log(data);
-      /*  res.json({
+        res.json({
         code: 200,
         operation: "success",
         description: "Fetched all posts",
         data: data.rows,
-      }); */
+      });
     } catch (e) {
       console.error(Error(e));
       res.status(500).send("A Team, there is a problem!");
