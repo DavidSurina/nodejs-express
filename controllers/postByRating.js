e4c19a7e31c6a790acb4edf1e00bcf3b452554a5
olkhon <alexander.stapel@gmail.com>
2021-02-03 08:26:59 +0100

    post by rating desc

--- controllers/postByRating.js
+++ controllers/postByRating.js
@@ -1,5 +1,21 @@
+const pool = require("../dbconfig");
+
 const getPostByRating = {
-  logPostByRating: (req, res) => res.send('rating test'),
+  logPostByRating: async (req, res) => {
+    try {
+      const data = await pool.query("SELECT * FROM posts ORDER BY rating DESC");
+      console.log(data);
+        res.json({
+        code: 200,
+        operation: "success",
+        description: "Fetched posts by Rating desc order",
+        data: data.rows,
+      });
+    } catch (e) {
+      console.error(Error(e));
+      res.status(500).send("A Team, there is a problem!");
+    }
+  },
 };
 
 module.exports = getPostByRating;
