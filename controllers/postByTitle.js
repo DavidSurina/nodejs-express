02a6411aae616a2cb98ffdbc4a72e018a13834db
memo40 <memocann4040@gmail.com>
2021-02-03 12:33:40 +0100

    postbytitle

--- controllers/postByTitle.js
+++ controllers/postByTitle.js
@@ -1,5 +1,19 @@
 const getPostByTitle = {
-    logPostByTitle: (req, res) => res.send('title test'),
+    logPostByTitle: (req, res) =>{
+    try {
+      const data = await pool.query("SELECT * FROM posts WHERE title");
+      console.log(data);
+        res.json({
+        code: 200,
+        operation: "success",
+        description: "Fetched all postsByTitle",
+        data: data.rows,
+      });
+    } catch (e) {
+      console.error(Error(e));
+      res.status(500).send("A Team, there is a problem!");
+    }
+  },
   };
 
   module.exports = getPostByTitle;
