343393e19174c3e2b6f75b0ee5c0168bb64e7ce6
Hermann Marx <marxhermann@web.de>
2021-02-04 15:34:00 +0100

    fixed issue

--- routes/posts.js
+++ routes/posts.js
@@ -3,15 +3,11 @@ const router = express.Router();
 
 const PostController = require("../controllers/postController");
 
-
-
-router.get("/:id", PostController.getById);
 router.get("/all", PostController.getAllPosts);
 router.get("/ratings", PostController.getRatings);
-router.get("/postbytopic/:topic", PostController.getByTopic);
 router.get("/postjoin", PostController.getPostJoin);
 router.get("/postbytitle/:title", PostController.getPostByTitle);
-
-
+router.get("/postbytopic/:topic", PostController.getByTopic);
+router.get("/:id", PostController.getById);
 
 module.exports = router;
